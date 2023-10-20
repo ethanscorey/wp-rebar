92cbbbb378b8e17a27a34b77ad684fa036f60579
Ethan Corey <ethanscorey@gmail.com>
2023-10-19 23:43:55 -0400

    Added initial config for ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// Based on config from Ali Alaa with convenience tweaks
+// https://github.com/alialaa/wp-blocks-course-plugin-boilerplate/blob/main/.eslintrc
+const config = {
+	"extends": [ "plugin:@wordpress/eslint-plugin/recommended-with-formatting", "prettier" ],
+	"rules": {
+		// Allow wrapping controls with labels
+		// See https://github.com/bradbirdsallCHANGED/eslint-plugin-jsx-a11y/blob/main/docs/rules/label-has-associated-control.md
+		"jsx-a11y/label-has-associated-control": [2, {
+			"assert": "either"
+		}]
+	}
+};
+
+module.exports = config;
